8d0e2d425a0abbb65b8555b5146e699134808c8b
kingwzun <1905952141@qq.com>
2022-12-19 12:50:41 +0800

    使用Spring JDBC简化Dao层操作

--- src/main/java/study/dao/impl/UserDaoImpl.java
+++ src/main/java/study/dao/impl/UserDaoImpl.java
@@ -1,135 +1,40 @@
 package study.dao.impl;
 
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import study.dao.IUserDao;
 import study.pojo.User;
 import study.utils.JDBCUtil;;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoImpl implements IUserDao {
+    private  JdbcTemplate template =new JdbcTemplate(JDBCUtil.getDataSource());
     @Override
     public List<User> selectAll() {
-        Connection connection = null;
-        PreparedStatement statement = null;
-        ResultSet resultSet = null;
-        List<User> list = new ArrayList<>();
-        try {
-            connection = JDBCUtil.getConnection();
-            String sql = "select id,name,password,email,phone from user";
-            statement = connection.prepareStatement(sql);
-            System.out.println(statement);
-            resultSet = statement.executeQuery();
-            while (resultSet.next()) {
-                int id = resultSet.getInt("id");
-                String name = resultSet.getString("name");
-                String password = resultSet.getString("password");
-                String email = resultSet.getString("email");
-                String phone = resultSet.getString("phone");
-
-                User user = new User();
-                user.setId(id);
-                user.setName(name);
-                user.setPassword(password);
-                user.setEmail(email);
-                user.setPhone(phone);
-
-                list.add(user);
-            }
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        } finally {
-            JDBCUtil.close(connection, statement, resultSet);
-        }
-
+        String sql = "select id,name,password,email,phone from user";
+        List<User> list = template.query(sql, new BeanPropertyRowMapper<User>(User.class));
         return list;
     }
 
     @Override
     public List<User> selectByPage(Integer offset, Integer limit) {
-        Connection connection = null;
-        PreparedStatement statement = null;
-        ResultSet resultSet = null;
-        List<User> list = new ArrayList<>();
-        try {
-            connection = JDBCUtil.getConnection();
-            String sql = "select id,name,password,email,phone from user limit ?,?";
-            statement = connection.prepareStatement(sql);
-            statement.setInt(1, offset);
-            statement.setInt(2, limit);
-            System.out.println(statement);
-            resultSet = statement.executeQuery();
-            while (resultSet.next()) {
-                int id = resultSet.getInt("id");
-                String name = resultSet.getString("name");
-                String password = resultSet.getString("password");
-                String email = resultSet.getString("email");
-                String phone = resultSet.getString("phone");
-
-                User user = new User();
-                user.setId(id);
-                user.setName(name);
-                user.setPassword(password);
-                user.setEmail(email);
-                user.setPhone(phone);
-
-                list.add(user);
-            }
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        } finally {
-            JDBCUtil.close(connection, statement, resultSet);
-        }
+        String sql = "select id,name,password,email,phone from user limit ?,?";
+        List<User> list = template.query(sql, new BeanPropertyRowMapper<User>(User.class), offset, limit);
         return list;
     }
 
     @Override
     public Long selectTotalCount() {
-        Connection connection = null;
-        PreparedStatement statement = null;
-        ResultSet resultSet = null;
-        int totalCount = 0;
-        try {
-            connection = JDBCUtil.getConnection();
-            String sql = "select count(*) from user";
-            statement = connection.prepareStatement(sql);
-            System.out.println(statement);
-            resultSet = statement.executeQuery();
-            while (resultSet.next()) {
-                totalCount = resultSet.getInt(1);
-            }
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        } finally {
-            JDBCUtil.close(connection, statement, resultSet);
-        }
-
-        return (long) totalCount;
+        String sql = "select count(*) from user";
+        Long totalCount = template.queryForObject(sql, Long.class);
+        return totalCount;
     }
 
     @Override
     public Integer deleteById(Integer id) {
-        Connection connection = null;
-        PreparedStatement statement = null;
-        int count = 0;
-        try {
-            connection = JDBCUtil.getConnection();
-            String sql = "delete from user where id=?";
-            statement = connection.prepareStatement(sql);
-            statement.setInt(1, id);
-            System.out.println(statement);
-            count = statement.executeUpdate();
-            System.out.println("count: " + count);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        } finally {
-            JDBCUtil.close(connection, statement, null);
-        }
-
+        String sql = "delete from user where id=?";
+        int count = template.update(sql, id);
         return count;
     }
 }
